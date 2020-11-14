3e5320e2ccbbc4cee8b7c34e9a81be67805c4085
imjoseangel <josea.munoz@gmail.com>
2020-11-15 00:24:33 +0100

    Add kubernetes context

--- powerline_k8sstatus/segments.py
+++ powerline_k8sstatus/segments.py
@@ -5,12 +5,35 @@ from __future__ import (division, absolute_import, print_function,
 
 from powerline.segments import Segment, with_docstring
 from powerline.theme import requires_segment_info
-from kubernetes.config import kube_config
+from kubernetes import config
 
 
 @requires_segment_info
 class K8SStatusSegment(Segment):
-    pass
+
+    def build_segments(self):
+        pass
+
+    def __call__(self, pl, segment_info, show_cluster=False, show_user=False, show_namespace=True):
+
+        try:
+            contexts, active_context = config.list_kube_config_contexts()
+        except config.config_exception.ConfigException:
+            return
+
+        if not contexts:
+            return
+
+        name = active_context['name']
+        cluster = active_context['context']['cluster']
+        user = active_context['context']['user']
+
+        try:
+            namespace = active_context['context']['namespace']
+        except KeyError:
+            return
+
+        return self.build_segments(active_context)
 
 
 k8sstatus = with_docstring(K8SStatusSegment(),
@@ -19,7 +42,9 @@ It will show the context name together with the cluster name and the namespace.
 :param bool show_cluster:
     Show cluster name. False by default to avoid long names.
 :param bool show_namespace:
-    Show namespace. For default namespace it hiddes the name. True if namespace is not default.
+    Show namespace. True if namespace is not `default`.
+:param bool show_user:
+    Show cluster connected user. False by default.
 Divider highlight group used: ``k8sstatus:divider``.
-Highlight groups used: ``k8sstatus_cluster``, ``k8sstatus_namespace``, ``k8sstatus``.
+Highlight groups used: ``k8sstatus_name``, ``k8sstatus_cluster``, ``k8sstatus_namespace``, ``k8sstatus_user``, ``k8sstatus``.
 ''')
