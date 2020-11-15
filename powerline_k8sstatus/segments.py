b8a1d6889893163747d6aa85aa60dce9e22adcd5
imjoseangel <josea.munoz@gmail.com>
2020-11-15 18:36:02 +0100

    Add context working

--- powerline_k8sstatus/segments.py
+++ powerline_k8sstatus/segments.py
@@ -1,25 +1,24 @@
-# -*- coding: utf-8 -*-
-
-from __future__ import (division, absolute_import, print_function,
-                        unicode_literals)
+# vim:fileencoding=utf-8:noet
+from __future__ import (unicode_literals, division,
+                        absolute_import, print_function)
 
 from powerline.segments import Segment, with_docstring
-from powerline.theme import requires_segment_info
+from powerline.theme import requires_segment_info, requires_filesystem_watcher
 from kubernetes import config
 
 
+@requires_filesystem_watcher
 @requires_segment_info
 class K8SStatusSegment(Segment):
+    divider_highlight_group = None
 
-    def build_segments(self, formats, context):
-        segments = [
-            {'contents': formats.get('k8sstatus', u'\U00002388  {}').format(
-                context), 'divider_highlight_group': 'k8sstatus:divider'}
-        ]
+    def build_segments(self, context):
+        segments = [{'contents': (u'\U00002388 {}').format(
+            context), 'highlight_groups': ['k8sstatus']}]
 
         return segments
 
-    def __call__(self, pl, segment_info, formats={}):
+    def __call__(self, pl, segment_info, create_watcher):
 
         try:
             contexts, active_context = config.list_kube_config_contexts()
@@ -31,19 +30,11 @@ class K8SStatusSegment(Segment):
 
         context = active_context['name']
 
-        return self.build_segments(formats, context)
+        return self.build_segments(context)
 
 
 k8sstatus = with_docstring(K8SStatusSegment(),
-'''Return the status of the current Kubernetes context.
+                           '''Return the status of the current Kubernetes context.
 
 It will show the context name together with the cluster name and the namespace.
-
-:param dict formats:
-    A string-to-string dictionary for customizing K8S status formats. Valid keys include ``context``.
-    Empty dictionary by default, which means the default formats are used.
-
-Divider highlight group used: ``k8sstatus:divider``.
-
-Highlight groups used: ``k8sstatus_name``, ``k8sstatus``.
 ''')
