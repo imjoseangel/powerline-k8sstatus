6276f756a00df03f46400333b36766126d4e9f36
Jose Angel Munoz <5019972+imjoseangel@users.noreply.github.com>
2020-12-12 22:25:19 +0100

    feat(setup.py): add clean feature (#21)
    Signed-off-by: imjoseangel <josea.munoz@gmail.com>

--- setup.py
+++ setup.py
@@ -1,4 +1,47 @@
-from setuptools import setup
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import (division, absolute_import, print_function,
+                        unicode_literals)
+
+
+import glob
+import os
+from os.path import abspath, dirname, normpath
+import shutil
+from setuptools import setup, Command
+
+
+here = normpath(abspath(dirname(__file__)))
+
+
+class CleanCommand(Command):
+    """Custom clean command to tidy up the project root."""
+    CLEAN_FILES = './build ./dist ./*.pyc ./*.tgz src/*.egg-info'.split(' ')
+
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        global here
+
+        for path_spec in self.CLEAN_FILES:
+            # Make paths absolute and relative to this path
+            abs_paths = glob.glob(os.path.normpath(
+                os.path.join(here, path_spec)))
+            for path in [str(p) for p in abs_paths]:
+                if not path.startswith(here):
+                    # Die if path in CLEAN_FILES is absolute + outside this directory
+                    raise ValueError(
+                        "%s is not a path inside %s" % (path, here))
+                print('removing %s' % os.path.relpath(path))
+                shutil.rmtree(path)
+
 
 setup(
     name='powerline-k8sstatus',
@@ -23,5 +66,8 @@ setup(
     ],
     install_requires=[
         "kubernetes"
-    ]
+    ],
+    cmdclass={
+        'clean': CleanCommand,
+    }
 )
